096bed0d80eaac4d4679845a77d81e2393846868
Moctar Diallo <moctarjallo@gmail.com>
2024-03-16 03:19:34 +0000

    Initial commit

---
+++ Template/agriculture/js/custom.js
@@ -0,0 +1,205 @@
+/*
+Vizion-Agriculture-Digital Marketing Responsive HTML5 Template
+Author: iqonicthemes.in
+Version: 1.0
+Design and Developed by: iqonicthemes.in
+*/
+
+/*----------------------------------------------
+Index Of Script
+------------------------------------------------
+
+Page Loader
+Back To Top
+Accordion
+Header
+Progress Bar
+skrollr
+Magnific Popup
+counter
+Wow Animation
+Countdown
+Owl Carousel
+
+------------------------------------------------
+Index Of Script
+----------------------------------------------*/
+(function($) {
+
+    "use strict";
+    $(document).ready(function() {
+
+        /*------------------------
+        Page Loader
+        --------------------------*/
+        jQuery("#load").fadeOut();
+        jQuery("#loading").delay(0).fadeOut("slow");
+
+        $(".navbar a").on("click", function(event) {
+            if (!$(event.target).closest(".nav-item.dropdown").length) {
+                $(".navbar-collapse").collapse('hide');
+            }
+        });
+
+        /*------------------------
+        Back To Top
+        --------------------------*/
+        $('#back-to-top').fadeOut();
+        $(window).on("scroll", function() {
+            if ($(this).scrollTop() > 250) {
+                $('#back-to-top').fadeIn(1400);
+            } else {
+                $('#back-to-top').fadeOut(400);
+            }
+        });
+        // scroll body to 0px on click
+        $('#top').on('click', function() {
+            $('top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+
+
+        /*------------------------
+        Accordion
+        --------------------------*/
+        $('.iq-accordion .iq-ad-block .ad-details').hide();
+        $('.iq-accordion .iq-ad-block:first').addClass('ad-active').children().slideDown('slow');
+        $('.iq-accordion .iq-ad-block').on("click", function() {
+            if ($(this).children('div').is(':hidden')) {
+                $('.iq-accordion .iq-ad-block').removeClass('ad-active').children('div').slideUp('slow');
+                $(this).toggleClass('ad-active').children('div').slideDown('slow');
+            }
+        });
+
+
+        /*------------------------
+        Header
+        --------------------------*/
+        $(window).on('scroll', function() {
+            if ($(this).scrollTop() > 100) {
+                $('header').addClass('menu-sticky');
+            } else {
+                $('header').removeClass('menu-sticky');
+            }
+        });
+
+        /*------------------------
+         Progress Bar
+         --------------------------*/
+        jQuery('.iq-progress-bar > span').each(function() {
+            var jQuerythis = jQuery(this);
+            var width = jQuery(this).data('percent');
+            jQuerythis.css({
+                'transition': 'width 2s'
+            });
+            setTimeout(function() {
+                jQuerythis.appear(function() {
+                    jQuerythis.css('width', width + '%');
+                });
+            }, 500);
+        });
+
+
+        /*------------------------
+         skrollr
+         --------------------------*/
+        skrollr.init({
+            forceHeight: false
+        });
+
+
+        /*------------------------
+        Magnific Popup
+        --------------------------*/
+
+        $('.popup-gallery').magnificPopup({
+            delegate: 'a.popup-img',
+            tLoading: 'Loading image #%curr%...',
+            type: 'image',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                navigateByImgClick: true,
+                enabled: true,
+                preload: [0, 1]
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+            }
+        });
+
+        $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+            type: 'iframe',
+            disableOn: 700,
+            mainClass: 'mfp-fade',
+            preloader: false,
+            removalDelay: 160,
+            fixedContentPos: false
+        });
+
+        /*------------------------
+        Wow Animation
+        --------------------------*/
+        var wow = new WOW({
+            boxClass: 'wow',
+            animateClass: 'animated',
+            offset: 0,
+            mobile: false,
+            live: true
+        });
+        wow.init();
+
+        /*------------------------
+            Countdown
+           --------------------------*/
+        $('#countdown').countdown({
+            date: '10/01/2020 23:59:59',
+            day: 'Day',
+            days: 'Days'
+        });
+
+        /*------------------------
+        Owl Carousel
+        --------------------------*/
+        $('.owl-carousel').each(function() {
+            var $carousel = $(this);
+            $carousel.owlCarousel({
+                items: $carousel.data("items"),
+                loop: $carousel.data("loop"),
+                margin: $carousel.data("margin"),
+                nav: $carousel.data("nav"),
+                dots: $carousel.data("dots"),
+                autoplay: $carousel.data("autoplay"),
+                autoplayTimeout: $carousel.data("autoplay-timeout"),
+                navText: ['<i class="fa fa-angle-left fa-2x"></i>', '<i class="fa fa-angle-right fa-2x"></i>'],
+                responsiveClass: true,
+                responsive: {
+                    // breakpoint from 0 up
+                    0: {
+                        items: $carousel.data("items-mobile-sm")
+                    },
+                    // breakpoint from 480 up
+                    480: {
+                        items: $carousel.data("items-mobile")
+                    },
+                    // breakpoint from 786 up
+                    786: {
+                        items: $carousel.data("items-tab")
+                    },
+                    // breakpoint from 1023 up
+                    1023: {
+                        items: $carousel.data("items-laptop")
+                    },
+                    1199: {
+                        items: $carousel.data("items")
+                    }
+                }
+            });
+        });
+
+    });
+
+
+})(jQuery);
